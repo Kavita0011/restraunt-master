dd1d8570f25c1aba3c0fb1a962ca7ac16ba81065
Kavita Bisht <bishtkavita779@gmail.com>
2024-12-22 14:29:39 +0530

    updated on 22-12-24

--- api/order.php
+++ api/order.php
@@ -1,10 +1,17 @@
 <?php
 include('../includes/db.php');
 
+session_start();
+
+// Check if the user is logged in by verifying the session
+if (empty($_SESSION["user_email"])) {
+    die("<div class='container'>You are not logged in. Please <a href='../api/user/user-login.php'>log in</a>.</div>");
+} 
+// if()
 // if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // $user_id = $_POST['user_id'];
+    $user_id = $_POST['user_id'];
     $menu_id = $_REQUEST['item'];
-    // $quantity = $_POST['quantity'];
+    $quantity = $_POST['quantity'];
 
     // Fetch price from menu
     $sql = "SELECT price FROM Menu WHERE menu_id = $menu_id";
